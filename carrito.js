e520ebd2725b52dc372ff6fdb6f0b7cd6f6eaf2f
juan pacanchiaue <juanespacan07@gmail.com>
2026-05-04 11:23:51 -0500

    tienda virtual

--- carrito.js
+++ carrito.js
@@ -3,16 +3,51 @@ function toggleTheme() {
 }
 
 let carrito = [];
+const botonesAgregar = document.querySelectorAll('.add-to-card');
+const contador = document.getElementById('cart-count');
+const listaCarrito = document.getElementById('lista-carrito');
+const carritoContainer = document.getElementById('carrito-container');
+const cartIcon = document.getElementById('cart-icon');
 
-const cartCount = document.getElementById("cart-count");
-const cartItems = document.getElementById("cart-items");
-const cartModel = document.getElementById("cart-model");
-const closeCart = document.getElementById("close-cart");
-const checkoutButton = document.getElementById("checkout");
-const totalElemnt = document.getElementById("purchase-model");
-const closePurchase = document.getElementById("close-purchase")
+cartIcon.addEventListener('click', (e) => {
+    e.preventDefault();
+    carritoContainer.style.display = carritoContainer.style.display === 'none' ? 'block' : 'none';
+});
 
-document.querySelectorAll(". add-to-cart").forEach((button) => {
+botonesAgregar.forEach(boton => {
+    boton.addEventListener('click', (e) => {
+        e.preventDefault();
+        const card = e.target.closest('.juego-card');
+        
+        const juego = {
+            id: Date.now(),
+            titulo: card.querySelector('h3:nth-of-type(1)').innerText,
+            precio: card.querySelector('h3:nth-of-type(2)').innerText
+        };
 
-    button.addEventListener("click")
-})
+        carrito.push(juego);
+        renderizarCarrito();
+    });
+});
+
+
+function renderizarCarrito() {
+    
+    listaCarrito.innerHTML = '';
+    
+    carrito.forEach(juego => {
+        const li = document.createElement('li');
+        li.innerHTML = `${juego.titulo} - ${juego.precio} <button onclick="quitar(${juego.id})">❌</button>`;
+        listaCarrito.appendChild(li);
+    });
+
+  
+    contador.innerText = carrito.length;
+}
+
+window.quitar = function(id) {
+    carrito = carrito.filter(juego => juego.id !== id);
+    renderizarCarrito();
+};
+
+
